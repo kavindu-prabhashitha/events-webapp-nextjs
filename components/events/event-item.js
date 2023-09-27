f6c6ab63bac8ee6c70a9a3e319a8ee8ee58efbe0
Kavindu Prabhashitha <donprabhashitha@gmail.com>
2023-09-28 00:59:41 +0530

    event list, event detail feature implemented

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,8 @@
-import Link from "next/link";
+import classes from "./event-item.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -9,23 +13,29 @@ function EventItem(props) {
     year: "numeric",
   });
 
-  const formattedAddress = location.relace(",", "\n");
-  const exploreLink = `/events/${id}`
+  const formattedAddress = location.replace(",", "\n");
+  const exploreLink = `/events/${id}`;
   return (
-    <li>
-      <img src={'/'+ image} alt="" />
-      <div>
-        <div>
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.title}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+    
+          <div className={classes.address}>
+          <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
-          <Link href={exploreLink}>Explore Event</Link>
+        <div className={classes.actions}>
+           <Button link={exploreLink}>
+            <span>Explorer Event</span>
+            <span className={classes.icon}><ArrowRightIcon /></span>
+            </Button>
         </div>
       </div>
     </li>
