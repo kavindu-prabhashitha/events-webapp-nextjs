7ff6224a118cfdbdec10796628737af0db2fdf96
Kavindu Prabhashitha <donprabhashitha@gmail.com>
2023-09-28 01:25:43 +0530

    event filter feature

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import EventList from "../components/events/event-list"
+import EventSearch from "../components/events/events-search";
 import { getFeaturedEvents } from "../dummy-data"
 
 function HomePage(){
     const featuredEvents = getFeaturedEvents();
  return (
     <div>
+        <EventSearch />
         <EventList items={featuredEvents} />
     </div>
  )
