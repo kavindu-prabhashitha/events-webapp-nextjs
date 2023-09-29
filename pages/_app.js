d4fc52e56b1998326b216414efe8dae37820167f
Kavindu Prabhashitha <donprabhashitha@gmail.com>
2023-09-30 00:53:10 +0530

    app wide context added

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
-import Layout from '../components/layout/layout'
-import '../styles/global.css'
+import Layout from "../components/layout/layout";
+import "../styles/global.css";
+import Notification from "../components/ui/notification";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <NotificationContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+        
+      </Layout>
+    </NotificationContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
